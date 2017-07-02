7a1a4aa977af7958a94f23df605f5fcc6ddbdc3a
Prashanth Madi <prashanthrmadi@gmail.com>
2017-07-02 02:42:40 -0500

    :package: removing cam module and using  exec method

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 var azure = require('azure-storage');
 var fs = require('fs');
-const PiCamera = require('pi-camera');
-const myCamera = new PiCamera({
-  mode: 'photo',
-  output: `${ __dirname }/test.jpg`,
-  width: 2592,
-  height: 1944,
-  nopreview: true,
-});
+const spawn = require('child_process').spawn;
+var moment = require('moment');
+
+var currentMillis = moment().valueOf();
 
 var storageConnString = "DefaultEndpointsProtocol=https;AccountName=prmadistorage;AccountKey=OrCEJR9PVtGIJ7+hi1YxY7jgOQ/YLoFr/5qCBFm76NtrC2Fxbbkl5qNOhxoXrkmqsPv9LPTQ8mhv0M9D6vKZaA==;EndpointSuffix=core.windows.net";
 var blobService = azure.createBlobService(storageConnString);
+var camera = spawn( 'raspistill', [ '-o', "./photos/"+currentMillis+".png" ] );
 
-myCamera.snap()
-  .then((result) => {
-    console.log(result);
-    fs.createReadStream('test.jpg').pipe(blobService.createWriteStreamToBlockBlob('photos', 'test.jpg'));
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+camera.stdout.on( 'data', data => {
+    console.log( `stdout: ${data}` );
+    // fs.createReadStream('test.jpg').pipe(blobService.createWriteStreamToBlockBlob('photos', 'test.jpg'));
+});
+
+camera.stderr.on( 'data', data => {
+    console.log( `stderr: ${data}` );
+});
