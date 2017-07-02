562d400e146eb242a03a602b301fe5bfb33b7276
Prashanth Madi <prashanthrmadi@gmail.com>
2017-07-02 02:02:36 -0500

    using pi-camera

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-var request = require('request');
 var azure = require('azure-storage');
 var fs = require('fs');
+const PiCamera = require('pi-camera');
+const myCamera = new PiCamera({
+  mode: 'photo',
+  output: `${ __dirname }/test.jpg`,
+  width: 2592,
+  height: 1944,
+  nopreview: true,
+});
+
 var storageConnString = "DefaultEndpointsProtocol=https;AccountName=prmadistorage;AccountKey=OrCEJR9PVtGIJ7+hi1YxY7jgOQ/YLoFr/5qCBFm76NtrC2Fxbbkl5qNOhxoXrkmqsPv9LPTQ8mhv0M9D6vKZaA==;EndpointSuffix=core.windows.net";
 var blobService = azure.createBlobService(storageConnString);
-fs.createReadStream('test.jpg').pipe(blobService.createWriteStreamToBlockBlob('photos', 'test.jpg'));
 
+myCamera.snap()
+  .then((result) => {
+    console.log(result);
+    fs.createReadStream('test.jpg').pipe(blobService.createWriteStreamToBlockBlob('photos', 'test.jpg'));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
