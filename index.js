51a6badcf982abb109a611e9f3ef53e1a0383c81
Prashanth Madi <prashanthrmadi@gmail.com>
2017-07-02 03:18:31 -0500

    :ski:  code optimization

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const spawn = require('child_process').spawn;
 var moment = require('moment');
 
 var currentMillis = moment().valueOf();
-
+var outputdir = __dirname + "/photos/";
 var storageConnString = "DefaultEndpointsProtocol=https;AccountName=prmadistorage;AccountKey=OrCEJR9PVtGIJ7+hi1YxY7jgOQ/YLoFr/5qCBFm76NtrC2Fxbbkl5qNOhxoXrkmqsPv9LPTQ8mhv0M9D6vKZaA==;EndpointSuffix=core.windows.net";
-var blobService = azure.createBlobService(storageConnString);
-var camera = spawn( 'raspistill', [ '-o', "./photos/"+currentMillis+".png" ] );
 
-camera.stdout.on( 'data', data => {
-    console.log( `stdout: ${data}` );
-    // fs.createReadStream('test.jpg').pipe(blobService.createWriteStreamToBlockBlob('photos', 'test.jpg'));
-});
+if (!fs.existsSync(outputdir)) {
+    fs.mkdirSync(outputdir);
+}
 
-camera.stderr.on( 'data', data => {
-    console.log( `stderr: ${data}` );
-});
+function takePicture() {
+    var blobService = azure.createBlobService(storageConnString);
+    var outputfile = currentMillis + ".png";
+    var camera = spawn('raspistill', ['-o', outputdir + outputfile]);
+
+    camera.stdout.on('data', data => {
+        fs.createReadStream(outputdir + outputfile).pipe(blobService.createWriteStreamToBlockBlob('photos', outputfile));
+    });
+
+    camera.stderr.on('data', data => {
+        console.log(`stderr: ${data}`);
+    });
+}
+
+setInterval(takePicture, 300000);
