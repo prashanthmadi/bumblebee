eb2c5c17b6e0be9b9df8311c492ccdbbf50a6975
Prashanth Madi <prashanthrmadi@gmail.com>
2017-07-08 00:09:14 -0500

    :pear:  keeping it simple with multiple servos sweeping

--- servo.js
+++ servo.js
@@ -6,18 +6,8 @@ const board = new five.Board({
 
 board.on("ready", function() {
 
- // Create a new `servos` hardware instance.
-  var servos = new five.Servos('P1-32','P1-12');
-
-  // Create a new `animation` instance.
-  var animation = new five.Animation(servos);
-
-  // Enqueue an animation segment with options param
-  // See Animation example and docs for details
-  animation.enqueue({
-    cuePoints: [0, 0.25, 0.75, 1],
-    keyFrames: [90, { value: 180, easing: "inQuad" }, { value: 0, easing: "outQuad" }, 90],
-    duration: 2000
-  });
+ // Create a new `servos` hardware instances.
+  var servos = new five.Servos('P1-32','P1-33');
+  servos.sweep();
 
 });
